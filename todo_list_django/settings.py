9a66af9c9fbce32ae34a5bf8a5e79569d2db4b97
Conrad Coyle <coylec@users.noreply.github.com>
2017-07-16 17:01:01 +0100

    Security & docs

--- todo_list_django/settings.py
+++ todo_list_django/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '&7jji+e57=t!453urvwgaad6+qj7s%uq-8mju64+@=))*67697'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "<your heroku app>.herokuapp.com"]
 
 
 # Application definition
@@ -38,8 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'tasks.apps.TasksConfig',
     'rest_framework',
+    'accounts',
+    'rest_framework_docs',
 ]
 
 REST_FRAMEWORK = {
@@ -66,7 +69,15 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:8080',
+    'http://127.0.0.1:8080',
+)
 
 ROOT_URLCONF = 'todo_list_django.urls'
 
@@ -138,3 +149,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
